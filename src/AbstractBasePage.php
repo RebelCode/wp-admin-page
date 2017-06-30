28e2f792df992ef2e39534462ad33ad5508a24dc
Miguel Muscat <miguelmuscat93@gmail.com>
2017-06-30 14:57:41 +0200

    Added `AbstractBasePage`

---
+++ src/AbstractBasePage.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace RebelCode\WordPress\Admin\Page;
+
+/**
+ * Base functionality for a WordPress admin page.
+ *
+ * @since [*next-version*]
+ */
+abstract class AbstractBasePage extends AbstractPage implements PageInterface
+{
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param string  $id      The page ID.
+     * @param string $title   The page title.
+     */
+    public function __construct($id, $title)
+    {
+        $this->_setId($id)
+             ->_setTitle($title)
+             ->_construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->_getId();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTitle()
+    {
+        return $this->_getTitle();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getContent()
+    {
+        return $this->_getContent();
+    }
+}
